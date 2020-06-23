80ba5b4e65c8ff10c38216954f9bffa035e3fe3a
peng <123456>
2020-06-23 23:37:30 +0800

    网关路由zuul改为spring cloud gateway

---
+++ thirteen-gateway/src/main/java/org/thirteen/gateway/GatewayAppilcation.java
@@ -0,0 +1,19 @@
+package org.thirteen.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aaron.Sun
+ * @description 网关路由模块启动类
+ * @date Created in 22:09 2020/6/23
+ * @modified By
+ */
+@SpringBootApplication
+public class GatewayAppilcation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAppilcation.class, args);
+    }
+
+}
