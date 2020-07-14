7a4484dd306769d5f08bb8bbc66bd501d62f9b64
孙鹏 <peng_zzu@163.com>
2020-07-14 17:44:08 +0800

    删除测试zipkin代码

--- thirteen-gateway/src/main/java/org/thirteen/gateway/GatewayAppilcation.java
+++ thirteen-gateway/src/main/java/org/thirteen/gateway/GatewayAppilcation.java
@@ -2,10 +2,6 @@ package org.thirteen.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Aaron.Sun
@@ -16,22 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class GatewayAppilcation {
 
-    @Bean
-    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
-    }
-
-    @Bean
-    public ClientHttpRequestFactory clientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setReadTimeout(5000);
-        return factory;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayAppilcation.class, args);
     }
-
 
 }
